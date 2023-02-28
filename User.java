857897736bbbd2511f9e4d7eaa2eb03fd9aca9d2
jonasKovacs <jonas.kovacs.1@gmail.com>
2023-02-28 17:05:08 -0500

    user

--- User.java
+++ User.java
@@ -1,3 +1,23 @@
+import java.util.Date;
+import java.util.UUID;
+
 public abstract class User {
+    protected UUID id;
+    protected String firstName; 
+    protected String lastName;
+    protected String email;
+    protected String username;
+    protected String password;
+    protected Date DoB;
+
+    public User(UUID id, String firstName, String lastName, String email,
+        String username, String password, Date DoB) { // loading from json
+        
+    }
     
+    public User(String firstName, String lastName, String email,
+        String username, String password, Date DoB) { // not loading from json
+        
+
+    }
 }
