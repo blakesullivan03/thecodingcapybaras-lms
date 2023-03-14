af56f7a927d7f39dcdbe7501f6d9f08ccc18964e
blakesullivan03 <blakest@email.sc.edu>
2023-03-14 14:44:09 -0400

    working out the data loader and the data constants

--- Question.java
+++ Question.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class Question {
     private String question;
     private ArrayList<String> answers;
-    private Integer correctAnswer;
+    private Long correctAnswer;
 
-    public Question(String question, ArrayList<String> answers, Integer correctAnswers){
+    public Question(String question, ArrayList<String> answers, Long correctAnswers){
         this.question = question;
         this.answers = answers;
         this.correctAnswer = correctAnswers;
