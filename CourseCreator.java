2b64ccdfc6ab27c0a57e54ec3ffdb4a84aceaca3
Michael <mikiejh@gmail.com>
2023-03-30 14:03:10 -0400

    Adding java doc to coursecreator and courseprofile

--- CourseCreator.java
+++ CourseCreator.java
@@ -2,31 +2,69 @@ import java.util.UUID;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * A user who is a course creator, with all their responsibilities
+ */
 public class CourseCreator extends User{
     private ArrayList<Course> courseArrayList;
     // private ArrayList<Course> courseArrayList = new ArrayList<>();
     // pretty sure without this it will output Nullpointerexception.
+    /**
+     * Creating a new course creator with overriden parameters for later use
+     * @param firstName the course creator's first name
+     * @param lastName the course creator's last name
+     * @param email the course creator's email
+     * @param password the course creator's password
+     * @param DoB the course creator's Date of Birth
+     * @param type the type of account (course creator)
+     */
     public CourseCreator(String firstName, String lastName, String email, String password, Date DoB, String type){
         super(firstName, lastName, email,password, DoB, type);
     }
+    /**
+     * Creating a new course creator with the added parameter of id.
+     * @param id a unique id assigned to a user
+     * @param firstName the course creator's first name
+     * @param lastName the course creator's last name
+     * @param email the course creator's email
+     * @param password the course creator's password
+     * @param DoB the course creator's Date of Birth
+     * @param type the type of account (course creator)
+     */
     public CourseCreator(UUID id, String firstName, String lastName, String email, String password, Date DoB, String type){
         super(id, firstName, lastName, email,password, DoB, type);
 
     }
 
+    /**
+     * Adding a course to the list of courses
+     * @param course the course that is being added
+     * @return the list of courses in existence
+     */
     public ArrayList<Course> addCourse(Course course){
         courseArrayList.add(course);
         return courseArrayList;
     }
 
+    /**
+     * Getting the courses
+     * @return the existing courses
+     */
     public ArrayList<Course> getCourse(){
         return courseArrayList;
     }
 
+   /* 
     public boolean isMyCourse(){
         return true;
     }
+*/
 
+    /**
+     * Displaying the course creator's information
+     * @return the course creator's information if there is one
+     * or a blank string if there is not a course creator.
+     */
     public String toString(){
         UserList users = UserList.getInstance();
 		ArrayList<CourseCreator> courseCreators = users.getCourseCreators();
--- CourseProfile.java
+++ CourseProfile.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 
+/**
+ * A profile for a user's courses
+ */
 public class CourseProfile {
     private Course course;
     private ArrayList<Double> moduleGrades;
@@ -7,34 +10,65 @@ public class CourseProfile {
     private Student student;
     private ArrayList<Module> modules;
 
+    /**
+     * Creating a new course profile with overloaded parameters for later use
+     * @param course the course the student is in
+     * @param student the student taking the course
+     */
     public CourseProfile(Course course, Student student) {
         this.course = course;
         this.student = student;
         this.moduleGrades = new ArrayList<Double>();
     }
 
+    /**
+     * Creating a new course profile
+     * @param course the course the student is in
+     * @param student the student taking the course
+     * @param moduleGrades the grades the student gets on the modules
+     */
     public CourseProfile(Course course, Student student,  ArrayList<Double> moduleGrades) { // from json
         this.course = course;
         this.student = student;
         this.moduleGrades = moduleGrades;
     }
 
+    /**
+     * Getting the grade for the course
+     * @return the course grade
+     */
     public double getGrade(){
         return this.courseGrade;
     }
 
+    /**
+     * Getting the list of grades for the different modules
+     * @return the module grades
+     */
     public ArrayList<Double> getGrades(){
         return this.moduleGrades;
     }
 
+    /**
+     * Getting the student
+     * @return the student
+     */
     public Student getStudent(){
         return this.student;
     }
 
+    /**
+     * Getting the course
+     * @return the course
+     */
     public Course getCourse() {
         return this.course;
     }
 
+    /**
+     * Entering the overall grade to the course profile
+     * @param grade the overall grade
+     */
     public void enterGrade(double grade){
         
         moduleGrades.add(grade);
@@ -43,10 +77,19 @@ public class CourseProfile {
 
     }
 
+    /**
+     * The certificate for users who complete a course 
+     * with above an 80%
+     */
     public static void certificate(){
         DataWriter.certificateFile();
     }
 
+    /**
+     * The course profile display
+     * @return the information for the course profile if there is information
+     * or incomplete if the course is not complete.
+     */
     public String toString() {
         modules = course.getModules();
         String ret =  "Course: " + course.getTitle() + " Grade: " + courseGrade + "\nModule Grades:  ";
