76eec0f03b5f5b7d8d51b4274e508024619d45f3
Nicole Fowler <mattfowler1515@yahoo.com>
2023-03-30 11:23:12 -0400

    UserList JDoc

--- UserList.java
+++ UserList.java
@@ -8,18 +8,37 @@ public class UserList {
 
     private ArrayList<Student> students;
     private ArrayList<CourseCreator> courseCreators;
-
+    /**
+     * Constructs a new UserList object.
+     * The list of users is initialized by loading data from the DataLoader class.
+     * */
     private UserList(){
         users = DataLoader.getUsers();
     }
-
+    /**
+     * Returns the instance of the UserList class.
+     * If the instance has not been created, it creates a new instance.
+     * @return the instance of the UserList class
+     **/
     public static UserList getInstance(){
         if(userList == null) {
 			userList = new UserList();
         }
 		return userList;
     }
-
+    /**
+     * Adds a new student to the list of users.
+     * If the user with the same email already exists in the list, the method returns null.
+     * @param firstName the first name of the new student
+     * @param lastName the last name of the new student
+     * @param email the email of the new student
+     * @param password the password of the new student
+     * @param DoB the date of birth of the new student
+     * @param overallGPA the overall GPA of the new student
+     * @param favoriteLanguages the list of favorite languages of the new student
+     * @param type the account type of the new student
+     * @return the new student if added successfully, null otherwise
+     * */
     //overload 
     public User addStudent(String firstName, String lastName, String email, String password, Date DoB, double overallGPA, ArrayList<Language> favoriteLanguages, String type){
         if(haveUser(email)){
@@ -31,7 +50,17 @@ public class UserList {
         users.add(newStudent);
         return newStudent;
     }
-
+    /**
+     * Adds a new course creator to the list of users.
+     * If the user with the same email already exists in the list, the method returns null.
+     * @param firstName the first name of the new course creator
+     * @param lastName the last name of the new course creator
+     * @param email the email of the new course creator
+     * @param password the password of the new course creator
+     * @param DoB the date of birth of the new course creator
+     * @param type the account type of the new course creator
+     * @return the new course creator if added successfully, null otherwise
+     * */
     public User addCourseCreator(String firstName, String lastName, String email, String password, Date DoB, String type){
         if(haveUser(email)){
             return null;
@@ -42,11 +71,18 @@ public class UserList {
         users.add(newCourseCreator);
         return newCourseCreator;
     }
-    
+    /**
+     * Deletes a user from the list of users.
+     * @param user the user to be deleted
+     * */
     public void deleteUser(User user){
         //users.remove(user);
     }
-
+    /**
+     * Checks if a user with the given email already exists in the list of users.
+     * @param email the email to check
+     * @return true if a user with the given email exists, false otherwise
+     * */
     // can have the same name, same password, same DOB, but not the same email
     public boolean haveUser(String email) {
 		for(User user : users) {
@@ -67,6 +103,11 @@ public class UserList {
 		return false;
 	}
 */
+    /**
+     * Retrieves a User object based on its unique ID.
+     * @param id the unique ID of the User to be retrieved
+     * @return the User object corresponding to the given ID, or null if no such User exists
+     * */
     //Loop throught user list
     public User getUserByID(UUID id){
         for(User user : users) {
@@ -76,7 +117,11 @@ public class UserList {
 		}
         return null;
     }
-
+    /**
+     * Retrieves a User object based on its username.
+     * @param id the username of the User to be retrieved
+     * @return the User object corresponding to the given username, or null if no such User exists
+     * */
     public User getUser(String id){
         for(User user : users) {
 			if(user.getUserName().equalsIgnoreCase(id)) {
@@ -85,7 +130,12 @@ public class UserList {
 		}
 		return null;
     }
-
+    /**
+     * Retrieves a User object based on its email and password.
+     * @param email the email address of the User to be retrieved
+     * @param password the password of the User to be retrieved
+     * @return the User object corresponding to the given email and password, or null if no such User exists
+     * */
     public User getUser(String email, String password) {
         for(User user : users) {
 			if(user.getEmail().equalsIgnoreCase(email) && user.getPassword().equals(password)) {
@@ -94,6 +144,12 @@ public class UserList {
 		}
 		return null;
     }
+    /**
+     * Retrieves the account type of a User object based on its email and password.
+     * @param email the email address of the User whose account type is to be retrieved
+     * @param password the password of the User whose account type is to be retrieved
+     * @return the account type of the User corresponding to the given email and password, or null if no such User exists
+     * */
 
     public String getUserType(String email, String password) {
         for(User user : users) {
@@ -103,15 +159,26 @@ public class UserList {
 		}
 		return null;
     }
-
+    /**
+     * Retrieves a list of all User objects.
+     * @return an ArrayList containing all User objects in the system
+     * */
     public ArrayList<User> getUsers(){
         System.out.println(users);
         return users;
     }
+    /**
+     * Retrieves a list of all Student objects.
+     * @return an ArrayList containing all Student objects in the system
+     * */
 
     public ArrayList<Student> getStudents(){
         return students;
     }
+    /**
+     * Retrieves a list of all CourseCreator objects.
+     * @return an ArrayList containing all CourseCreator objects in the system
+     * */
     public ArrayList<CourseCreator> getCourseCreators(){
         return courseCreators;
     }
